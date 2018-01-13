9f5e9c386e7d9ae7007e6422217ba44e3132b085
Angus McLeod <angus@mcleod.org.au>
2018-01-13 11:35:00 +0800

    Add temporary clear for 'sidebar' widget

--- plugin.rb
+++ plugin.rb
@@ -5,5 +5,8 @@
 register_asset 'stylesheets/layouts-sidebar.scss'
 
 DiscourseEvent.on(:layouts_ready) do
+  # To be removed
+  DiscourseLayouts::WidgetHelper.clear_widget('sidebar')
+
   DiscourseLayouts::WidgetHelper.add_widget('user-sidebar', position: 'left', order: 'start')
 end
